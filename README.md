dac81ec82e057bd5b288ea98b826808abf64dc77
Michael Bridges <mike@michaelbridges.co.uk>
2018-05-17 16:31:42 +0100

    Standard Curses Setup, no wrapper = issues

--- README.md
+++ README.md
@@ -6,5 +6,15 @@ Adding Curses External Library
 Adding pyGame External Library
 NB, Try using both libraries will force a game logic and view layer separation
 
+## Created Requirements.txt
+In the terminal `pip install pygame`
+`pip freeze > requirements.txt`
 
-
+## Standard Curses Setup
+Removed pygame for the moment.
+Get game working, think about logic and view separately
+Then logic can be used with any UI on top? :)
+---
+Standard Curses setup Made-
+    but need a wrapper-
+    if something breaks then because of the curses setup you wont see any useful de-bugging
--- snake.py
+++ snake.py
@@ -1,8 +1,15 @@
-# Ideally the code should have a requirements.txt
-# This why the venv is up and running easily
-
-import pygame  # TODO- These won't work yet
 import curses
 
+# STANDARD CURSES SETUP:
+stdscr = curses.initscr()  # Creates a new window object
+curses.noecho()  # EXPLICT: do not update console with user input- it is usually ignored anyway)
+curses.cbreak()  # React to key input without requiring return
+stdscr.keypad(True)  # Allows special input like the arrow keys
 
 
+# To stop a curses application we reverse the above with:
+curses.nocbreak()
+stdscr.keypad(False)
+curses.echo()
+
+curses.endwin()  #  Returns the terminal/console to normality
