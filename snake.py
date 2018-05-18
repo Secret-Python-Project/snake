77716a26d9bd8e05dd7ad7bb7c4ee5830c3360aa
Michael Bridges <mike@michaelbridges.co.uk>
2018-05-18 22:19:22 +0100

    Set Window Size

--- snake.py
+++ snake.py
@@ -1,20 +1,20 @@
 import curses
 from curses import wrapper
 
-timer = 1000
 
-
-def main(stdscr):
+def main(game_window):
     x = 5
+    timer = 1000
     welcome_message = 'Beep every: ' + str(int(timer / 1000)) + ' second/s, ' + str(x) + ' times'
 
-    while x > 0:
-        win_h, win_w = stdscr.getmaxyx()  # NOTE that the height and width are in columns
+    win_h, win_w = 50, 50
+    game_window = curses.newwin(win_h, win_w)
 
-        stdscr.clear()  # Clear screen
-        stdscr.addstr(0, 0, welcome_message, curses.A_BOLD)
-        stdscr.addstr(2, 0, str(str(win_h) + ' ' + str(win_w)),)  # This looks messy am I missing something????
-        stdscr.refresh()  # Make sure that this goes before waiting for user inPut
+    while x > 0:
+        #game_window.clear()  # Clear screen
+        game_window.addstr(0, 0, welcome_message, curses.A_BOLD)
+        game_window.addstr(2, 0, str(str(win_h) + ' ' + str(win_w)),)  # This looks messy am I missing something????
+        game_window.refresh()  # Make sure that this goes before waiting for user inPut
 
         curses.beep()  # Just for fun
         curses.napms(timer)
