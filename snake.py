8324e6eab31be5eac55dc05d162cbab2323782ce
Michael Bridges <mike@michaelbridges.co.uk>
2018-05-22 17:00:23 +0100

    New Game loop based on lives made.

--- snake.py
+++ snake.py
@@ -2,7 +2,6 @@ import curses
 from curses import wrapper
 from random import randint
 
-
 play_space_h = 20
 play_space_w = 40
 timer = 2000
@@ -11,6 +10,7 @@ timer = 2000
 class Snake():  # A class is needed so that we can spawn a new snake object in our game, perhaps when Life Lost?
     head = '@'  # A class variable shared by all instances
     body = 'o'
+    lives = 3
     head_coords = int(play_space_h / 2), int(play_space_w / 2)
     head_y = head_coords[0]
     head_x = head_coords[1]
@@ -18,14 +18,15 @@ class Snake():  # A class is needed so that we can spawn a new snake object in o
     def snake_body(self):
         body_pieces = []  # Attribute specific to this snake
 
+
 def main(game_window):
     game_window = curses.newwin(play_space_h, play_space_w)
 
-    while True:
+    while Snake.lives > 0:
         game_window.clear()  # Clear screen before the loop means all elements have to load in every loop Good or Bad?  a
         game_window.box()  # Added a border
         game_window.addstr(Snake.head_coords[0], Snake.head_coords[1], Snake.head)
-        #make_food(game_window)  # Removed from running for the moment
+        # make_food(game_window)  # Removed from running for the moment
         move_snake_with_user_input(game_window)
 
 
@@ -42,24 +43,31 @@ def make_food(game_window):
 
 
 def move_snake_with_user_input(game_window):
-
     key = game_window.getch()
 
     if key == ord('w'):
         Snake.head_coords = [Snake.head_y - 1, Snake.head_x]
         Snake.head_y -= 1
+        check_for_collision(game_window)
 
     elif key == ord('s'):
         Snake.head_coords = [Snake.head_y + 1, Snake.head_x]
         Snake.head_y += 1
+        check_for_collision(game_window)
 
     elif key == ord('a'):
         Snake.head_coords = [Snake.head_y, Snake.head_x - 1]
         Snake.head_x -= 1
+        check_for_collision(game_window)
 
     elif key == ord('d'):
         Snake.head_coords = [Snake.head_y, Snake.head_x + 1]
         Snake.head_x += 1
+        check_for_collision(game_window)
+
+
+def check_for_collision(game_window):  # List of death conditions
+    return # TODO make this do something
 
 
 wrapper(main)
