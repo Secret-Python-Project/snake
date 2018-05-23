01dd303bea6d56c197db1ddbbdd9a87554b2dbe8
Michael Bridges <mike@michaelbridges.co.uk>
2018-05-23 11:08:35 +0100

    Responding to Input and printing direction

--- snake.py
+++ snake.py
@@ -5,30 +5,37 @@ from random import randint
 play_space_h = 20
 play_space_w = 40
 timer = 2000
-game_tick = 500
+game_tick = 200
 
 
-class Snake():  # A class is needed so that we can spawn a new snake object in our game, perhaps when Life Lost?
-    head = '@'  # A class variable shared by all instances
+class Snake:  # A class is needed so that we can spawn a new snake object in our game, perhaps when Life Lost?
+    head = 'O'  # A class variable shared by all instances
     body = 'o'
     lives = 3
     head_coords = int(play_space_h / 2), int(play_space_w / 2)
     head_y = head_coords[0]
     head_x = head_coords[1]
+    direction = 'up'  #  Choose between default direction or none
+
 
     def snake_body(self):
+
         body_pieces = []  # Attribute specific to this snake
 
 
-def main(game_window):
+def play_game(game_window):
     game_window = curses.newwin(play_space_h, play_space_w)
+    game_window.clear()  # Clear screen before the loop means all elements have to load in every loop Good or Bad?  a
+    game_window.box()  # Added a border
+    game_window.addstr(Snake.head_coords[0], Snake.head_coords[1], Snake.head)  # Starting position
 
-    while Snake.lives > 0:
-        game_window.clear()  # Clear screen before the loop means all elements have to load in every loop Good or Bad?  a
-        game_window.box()  # Added a border
+    while True:
+        get_user_direction(game_window)
+        keep_moving(game_window)
         game_window.addstr(Snake.head_coords[0], Snake.head_coords[1], Snake.head)
         # make_food(game_window)  # Removed from running for the moment
-        move_snake_with_user_input(game_window)
+        #keep_moving(direction)
+        #curses.napms(game_tick)
 
 
 def make_food(game_window):
@@ -43,29 +50,30 @@ def make_food(game_window):
         curses.napms(timer)
 
 
-def move_snake_with_user_input(game_window):
+def get_user_direction(game_window):
     key = game_window.getch()
-    valid_inputs = ['w', 'a', 's', 'd']
+
     if key == ord('w'):
-        Snake.head_coords = [Snake.head_y -1, Snake.head_x]
-        Snake.head_y -= 1
-        check_for_collision(game_window)
+        Snake.direction = 'up'
 
     elif key == ord('s'):
-        Snake.head_coords = [Snake.head_y + 1, Snake.head_x]
-        Snake.head_y += 1
-        check_for_collision(game_window)
+        Snake.direction = 'down'
+
 
     elif key == ord('a'):
-        Snake.head_coords = [Snake.head_y, Snake.head_x - 1]
-        Snake.head_x -= 1
-        check_for_collision(game_window)
+        Snake.direction = 'left'
+
 
     elif key == ord('d'):
-        Snake.head_coords = [Snake.head_y, Snake.head_x + 1]
-        Snake.head_x += 1
-        check_for_collision(game_window)
+        Snake.direction = 'right'
 
+
+def keep_moving(game_window):
+
+    while Snake.direction == 'up':
+        get_user_direction(game_window)
+        Snake.head_y -=1
+        print(Snake.direction)
 
 def check_for_collision(game_window):  # List of death conditions
     if Snake.head_x == 0 or Snake.head_x == play_space_w - 1 or Snake.head_y == 0 or Snake.head_y == play_space_h - 1:
@@ -74,4 +82,5 @@ def check_for_collision(game_window):  # List of death conditions
     return # TODO make this do something
 
 
-wrapper(main)
+if __name__ == "__main__":
+    wrapper(play_game)
